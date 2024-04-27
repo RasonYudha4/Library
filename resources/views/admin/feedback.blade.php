eea41d21f70c79b22daf7188bc6cc7496559f2c7
RasonYudha4 <Rasonyudha921@gmail.com>
2024-04-27 08:36:41 +0700

    Redesign the admin feedback

--- resources/views/admin/feedback.blade.php
+++ resources/views/admin/feedback.blade.php
@@ -1,4 +1,4 @@
-<div>
+<!-- <div>
     <h1>Feedbacks</h1>
     <div class="w-5/6 py-10">
         @foreach($feedbacks as $feedback)
@@ -25,4 +25,41 @@
         </div>
         @endforeach
     </div>
+</div> -->
+<x-app-layout>
+@section('content')
+<div class=" min-h-screen w-full bg-[#181818]">
+    <h1 class=" text-[#f7a317] font-extrabold text-5xl m-8">Feedbacks</h1>
+    <table class=" w-full mt-8">
+        <thead>
+            <tr>
+                <th class="text-[#f7a317] mr-6">Subject</th>
+                <th class="text-[#f7a317] mr-6">Body</th>
+                <th class="text-[#f7a317] mr-6">Email</th>
+                <th class="text-[#f7a317] mr-6">Delete</th>
+            </tr>
+        </thead>
+        <tbody class=" mt-7">
+            @foreach($feedbacks as $feedback)
+            <tr class="m-2">
+                <td class="text-[#f7a317] px-6">{{ $feedback->subject}}</td>
+                <td class="text-[#f7a317] px-6 text-ellipsis overflow-hidden max-w-80">{{ $feedback->body}}</td>
+                <td class="text-[#f7a317] px-6">{{$user->email}}</td>
+                <td>
+                    <form action="/admin/feedback/{{$feedback->id}" method="POST">
+                        @csrf 
+                        @method('delete')
+
+                        <button type="submit" onclick="return confirm('Are you sure you want to delete this book?')" class=" px-3 text-[#f7a317] hover:text-[#ea6820]">
+                            Delete
+                        </button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
+
+@endsection
+</x-app-layout>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -33,31 +33,6 @@
             @endforeach
         </tbody>
     </table>
-    <!-- <div class="w-5/6 py-10">
-        @foreach($books as $book)
-        <div>
-            <h3>
-                {{ $book->title}}
-            </h3>
-
-            <p>
-            {{ $book->content}}
-            </p>
-
-            <img src="{{ asset('images/' . $book->image_path)}}" alt="" class=" max-w-10 max-h-20">
-
-            <a href="{{$book->id}}/edit">Edit</a>
-            <form action="/admin/book/{{$book->id}}" method="POST">
-                @csrf 
-                @method('delete')
-
-                <button type="submit" onclick="return confirm('Are you sure you want to delete this book?')">
-                    Delete
-                </button>
-            </form>
-        </div>
-        @endforeach
-    </div> -->
 </div>
 
 @endsection
